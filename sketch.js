2547835a6f1aa2945731e9eb8c37f03d27fb2027
agnis <75524907+agn404@users.noreply.github.com>
2025-07-21 16:52:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,18 +2,15 @@ let x = 0;
 let speedSlider;
 
 function setup() {
-  createCanvas(600, 200);
-  background(255);
-  
-  // Create slider: min=0, max=10, start=2, step=0.1
+  createCanvas(windowWidth, windowHeight * 0.4); // 40% of screen height
   speedSlider = createSlider(0, 10, 2, 0.1);
-  speedSlider.position(10, height + 10);  // Place below canvas
+  speedSlider.position(10, height + 10);
 }
 
 function draw() {
   background(240);
-  
-  let speed = speedSlider.value();  // Get slider value
+
+  let speed = speedSlider.value();
   fill(50, 100, 255);
   ellipse(x, height / 2, 50, 50);
   x += speed;
@@ -21,4 +18,9 @@ function draw() {
   if (x > width) {
     x = 0;
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight * 0.4);
+  speedSlider.position(10, height + 10); // Reposition slider after resize
 }
